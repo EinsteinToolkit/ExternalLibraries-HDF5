bb4ffc26c1cd38c581738ad9c73eafa82dc7804d
eschnett <eschnett@fb53df36-e548-4a1e-8150-ab98cbd5e786>
2015-01-06 22:12:05 +0000

    Update to HDF5 1.8.14
    git-svn-id: http://svn.cactuscode.org/projects/ExternalLibraries/HDF5/trunk@101 fb53df36-e548-4a1e-8150-ab98cbd5e786

--- src/detect.sh
+++ src/detect.sh
@@ -132,7 +132,7 @@ then
 
     # Set locations
     THORN=HDF5
-    NAME=hdf5-1.8.13
+    NAME=hdf5-1.8.14
     SRCDIR="$(dirname $0)"
     BUILD_DIR=${SCRATCH_BUILD}/build/${THORN}
     if [ -z "${HDF5_INSTALL_DIR}" ]; then
